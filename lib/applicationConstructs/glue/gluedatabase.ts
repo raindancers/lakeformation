967c8bad39e62d57da2535ccf07640e26d7626d5
Andrew Frazer <andrew.frazer@raindancers.cloud>
2023-05-10 18:55:52 +1200

    added delays

--- lib/applicationConstructs/glue/gluedatabase.ts
+++ lib/applicationConstructs/glue/gluedatabase.ts
@@ -1,9 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
 import {
   aws_s3 as s3,
   aws_lakeformation as lakeformation,
   aws_iam as iam,
   aws_glue as glue,
+  custom_resources as cr,
+  aws_lambda as aws_lambda,
 } from 'aws-cdk-lib';
 
 import * as constructs from 'constructs';
@@ -47,9 +50,7 @@ export class GlueDataBase extends constructs.Construct {
   }
 
   public addCrawler(props: CrawlerProps): crawler.Crawler {
-  //public addCrawler(props: CrawlerProps): void {
-
-
+  
 	// allow the crawler to access to the glue database
 	new lakeformation.CfnPermissions(this, 'gluedatabasepermission', {
 		dataLakePrincipal: {
@@ -66,24 +67,30 @@ export class GlueDataBase extends constructs.Construct {
 
 	// if targets includes s3targets give permissions to them.
 
+	
+
+
 	if (props.targets.s3Targets) {
-		props.targets.s3Targets.forEach((target, index) => { 
+
+
+		props.targets.s3Targets.forEach((target, index) => {
 			
-			new lakeformation.CfnPermissions(this, `s3permission${index}`, {
+			const s3permission = new lakeformation.CfnPermissions(this, `s3permission${index}`, {
 				dataLakePrincipal: {
 					dataLakePrincipalIdentifier: props.crawlerRole.roleArn
 				},
 				resource: {
 					dataLocationResource: {
 						catalogId: this.database.catalogId,
-						s3Resource: `${target.s3Arn}` // this needs to be the S3Arn?
+						s3Resource: `${target.s3Arn}` 
 
 					},
 				},
 				permissions: [datalake.Permissions.CREATE_TABLE_READ_WRITE]
 			})
 		})
-	};
+	}
+
 
 	const dataCrawler = new crawler.Crawler(this, `crawler${props.name}`, {
 		name: props.name,
@@ -92,6 +99,7 @@ export class GlueDataBase extends constructs.Construct {
 		description: props.description,
 		targets: props.targets
 	})
+
 
 	return dataCrawler
   }
--- lib/stacks/datatake/theLake.ts
+++ lib/stacks/datatake/theLake.ts
@@ -1,14 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import * as path from 'path'
 import { 
   aws_s3 as s3, 
-  aws_iam as iam
+  aws_iam as iam,
 } from 'aws-cdk-lib';
 import * as constructs from 'constructs';
 import * as lakeformation from '../../applicationConstructs/lakeformation/lakeformation'
 import { S3Target } from "../../applicationConstructs/glue/crawler/s3Target";
 import { GlueDataBase } from "../../applicationConstructs/glue/gluedatabase";
-import { create } from 'domain';
-
+import { Delay } from '../../applicationConstructs/delay/delay';
 
 
 export interface LakeStackProps extends cdk.StackProps {
@@ -27,7 +27,7 @@ export class WorldDataLakeFormation extends cdk.Stack {
   constructor(scope: constructs.Construct, id: string, props: LakeStackProps) {
     super(scope, id, props);
 
-
+    // this code add is general lakeformation setup. 
     
     this.datalake = new lakeformation.LakeFormation(this, 'LakeFormation', {
       nonproduction: props.nonproduction,
@@ -36,20 +36,42 @@ export class WorldDataLakeFormation extends cdk.Stack {
 
     // create and add new buckets to the datalake to create Lake Locations
     this.bronze = this.datalake.addNewBucketToLakeFormation({
-      name: 'TESTbronze'
+      name: 'bronze'
     }); 
 
     this.silver = this.datalake.addNewBucketToLakeFormation({
-      name: 'TESTsilver'
+      name: 'silver'
     });
 
     this.gold = this.datalake.addNewBucketToLakeFormation({
-      name: 'TESTgold'
+      name: 'gold'
     });
+  
+
+    const waitforassn = new cdk.CustomResource(this, `delayresource`, {
+      serviceToken: new Delay(this, 'delay').delayProviderServiceToken,
+      properties: {
+        SleepTime: 60,
+      },
+    })
+    
+    // adding datalake locations is an async operation, so we can not
+    // rely on cloudformation to tell us that the resoruce is actually built
+    // start a delay, after all the locatitons claim they are finished.
+
+    waitforassn.node.addDependency(this.bronze)
+    waitforassn.node.addDependency(this.silver)
+    waitforassn.node.addDependency(this.gold)
+    
+
+
+    // the below code is specific for an ingest
 
     const database = new GlueDataBase(this, 'gluedatabase', {
       databaseName: "worlddata"
     })
+    // wait untill the delay is done before creating the database
+    database.node.addDependency(waitforassn)
     
 
     // create a Target for the Crawler to use
@@ -58,19 +80,18 @@ export class WorldDataLakeFormation extends cdk.Stack {
         bucket: this.bronze,
         path: 'ingest/worldData/',
       },
-      //connectionName: 'WorlddataIngest',
     })
 
+    // create a role for this crawler to use. 
     const crawlerRole = new iam.Role(this, 'crawlerRole', {
       assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
     });
   
-  
+    // add additonal permissions as required. 
     crawlerRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole'));
     crawlerRole.addToPolicy(
       new iam.PolicyStatement({
       actions: [
-        //'iam:PassRole',
         'lakeformation:GetDataAccess',
       ],
       effect: iam.Effect.ALLOW,
@@ -95,12 +116,8 @@ export class WorldDataLakeFormation extends cdk.Stack {
         AccountId: cdk.Aws.ACCOUNT_ID,
         UseLakeFormationCredentials: true  
       }
-    )
-    
+    )    
     crawler.setConfiguration("{\"Version\":1.0,\"CreatePartitionIndex\":true}") // this might be beetter set as an enum.
-    
-
-    
-
+    crawler.node.addDependency(waitforassn)
   }
 }
